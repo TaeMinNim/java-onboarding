68f7d0be8190a6575936a9b7ace622f616b8c09b
TaeMin <xoals3094@naver.com>
2022-12-08 15:51:39 +0900

    Feat : solution 메소드 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,13 @@
 package onboarding;
 
+import onboarding.Problem1Class.GameController;
+
 import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
+        GameController gameController = new GameController();
+        int answer = gameController.gameStart(pobi, crong);
         return answer;
     }
 }
