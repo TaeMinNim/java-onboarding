922fa9af2fda600f72bd480408617e24594ae3d4
TaeMin <xoals3094@naver.com>
2022-12-08 15:34:13 +0900

    Feat(Plyaer) : 유효값 검증 메소드 추가

--- src/main/java/onboarding/Problem1Class/Player.java
+++ src/main/java/onboarding/Problem1Class/Player.java
@@ -3,12 +3,35 @@ package onboarding.Problem1Class;
 import java.util.List;
 
 public class Player {
+    private final static int MAX_SIZE = 2;
+    private final static int MIN_PAGE = 1;
+    private final static int MAX_PAGE = 400;
     private final static int LEFT = 0;
     private final static int RIGHT = 1;
     private final List<Integer> page;
 
     public Player(List<Integer> page){
+        validData(page);
         this.page = page;
+    }
+
+    private void validData(List<Integer> page){
+        if(page.size() != MAX_SIZE){
+            throw new IllegalArgumentException("page 리스트는 크기가 2이어야 합니다");
+        }
+
+        int left = page.get(LEFT);
+        int right = page.get(RIGHT);
+        if(!(isValidPage(left) && isValidPage(right))){
+            throw new IllegalArgumentException("page는 1~400 사이의 값이어야 합니다");
+        }
+        if(right - left != 1){
+            throw new IllegalArgumentException("page는 오른쪽 페이지가 왼쪽 페이지보다 1만큼 커야 합니다");
+        }
+    }
+
+    private boolean isValidPage(int number){
+       return number >= MIN_PAGE && number <= MAX_PAGE;
     }
 
     public int getScore(){
