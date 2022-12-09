a7b30cbeee0b312274b35a0dbd50ace080427f69
TaeMinNim <xoals3094@naver.com>
2022-12-10 01:16:56 +0900

    Feat(Translator) : 문자열 인덱스를 하나씩 탐색하며 청개구리 문자열로 변환하는 기능 구현

--- src/main/java/onboarding/Problem4Class/Translator.java
+++ src/main/java/onboarding/Problem4Class/Translator.java
@@ -6,6 +6,15 @@ public class Translator {
     private final static int MIN_UPPER_CASE = 65;
     private final static int MAX_UPPER_CASE = 90;
 
+    public String toGreenFrogWord(String word){
+        StringBuffer stringBuffer = new StringBuffer();
+        for(int i = 0; i < word.length(); i++){
+            char greenFrogChar = toGreenFrogChar(word.charAt(i));
+            stringBuffer.append(greenFrogChar);
+        }
+
+        return stringBuffer.toString();
+    }
     private char toGreenFrogChar(char c){
         int ascii = (int)c;
         if(ascii >= MIN_LOWER_CASE && ascii <= MAX_LOWER_CASE){
