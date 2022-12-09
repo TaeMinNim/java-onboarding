ff4910fcfb1427aa95b091d936ef07efbd828a37
TaeMinNim <xoals3094@naver.com>
2022-12-10 00:23:44 +0900

    Fix(Counter) : matchCount 메소드 버그 수정

--- src/main/java/onboarding/Problem3Class/Counter.java
+++ src/main/java/onboarding/Problem3Class/Counter.java
@@ -4,7 +4,7 @@ public class Counter {
     public int matchCount(int number){
         int match = 0;
         for(; number > 0; number /= 10){
-            int digit = number %= 10;
+            int digit = number % 10;
             if(digit == 3 || digit == 6 || digit == 9){
                 match++;
             }
