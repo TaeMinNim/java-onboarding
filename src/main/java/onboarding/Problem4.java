f42fb9a0bbf3d2cdd4139caa6769b5e947ed13b1
TaeMinNim <xoals3094@naver.com>
2022-12-10 01:18:06 +0900

    Feat : 기능 결합

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,11 @@
 package onboarding;
 
+import onboarding.Problem4Class.Translator;
+
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
+        Translator translator = new Translator();
+        String answer = translator.toGreenFrogWord(word);
         return answer;
     }
 }
