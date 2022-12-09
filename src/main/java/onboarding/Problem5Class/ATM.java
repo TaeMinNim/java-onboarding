e8b54df0533af63130fc989af6acc0c7c63b79d1
TaeMinNim <xoals3094@naver.com>
2022-12-10 01:48:34 +0900

    Feat(ATM) : 단위 목록 리스트를 순환하며 money를 최대한 적은 화폐로 발행할 수 있도록 만드는 기능 구현

--- src/main/java/onboarding/Problem5Class/ATM.java
+++ src/main/java/onboarding/Problem5Class/ATM.java
@@ -1,14 +1,23 @@
 package onboarding.Problem5Class;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ATM {
     private int money;
-    private final List<Integer> units = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
+    private final static List<Integer> UNITS = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
     public ATM(int money){
         this.money = money;
     }
 
+    public List<Integer> publication(){
+        List<Integer> cash = new ArrayList<>();
+        for(int unit: UNITS){
+            cash.add(count(unit));
+        }
+
+        return cash;
+    }
     private int count(int unit){
         return money / unit;
     }
