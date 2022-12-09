e2fda678466196579490bca9a9cc13a0b9c8a60e
TaeMinNim <xoals3094@naver.com>
2022-12-10 01:52:38 +0900

    Feat : 기능 결합

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,14 @@
 package onboarding;
 
+import onboarding.Problem5Class.ATM;
+
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        ATM atm = new ATM(money);
+        List<Integer> answer = atm.publication();
         return answer;
     }
 }
