da07b7ead9b9b08c622bd16d9570d90733276b80
TaeMinNim <xoals3094@naver.com>
2022-12-09 23:41:54 +0900

    Feat(Game) : 숫자가 몇개의 3, 6, 9를 포함하고 있는지 확인하는 기능

--- src/main/java/onboarding/Problem3Class/Game.java
+++ src/main/java/onboarding/Problem3Class/Game.java
@@ -8,6 +8,18 @@ public class Game {
         this.number = number;
     }
 
+    private int matchCount(int number){
+        int match = 0;
+        for(; number > 0; number /= 10){
+            int digit = number %= 10;
+            if(digit == 3 || digit == 6 || digit == 9){
+                match++;
+            }
+        }
+
+        return match;
+    }
+
     private boolean lessThanNumber(){
         return count <= number;
     }
